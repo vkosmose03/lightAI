db324a7382240e633a6fcc3b250cfedcf53a5e92
vkosmose03 <v.makarov@orsyst.ru>
2026-04-19 21:25:40 +0300

    feat: basic learning protect

--- core/layer.cpp
+++ core/layer.cpp
@@ -8,6 +8,7 @@ layer::layer(int n, int m,
 {
     double scale = std::sqrt(2.0 / static_cast<double>(m));
     W_ = Eigen::MatrixXd::Random(n, m) * scale;
+    W_ = (Eigen::MatrixXd::Random(n, m).array() + 1.0) / 2.0;
     b_ = Eigen::VectorXd::Zero(n);
     z_.resize(n); output_.resize(n); input_.resize(m);
 }
